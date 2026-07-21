e1fbba210197e9a9361ff8704c06854586bac2cc
EdenReid <eden.reid.2315@gmail.com>
2026-07-21 12:18:57 +0100

    basic UI question page

--- app.py
+++ app.py
@@ -1,6 +1,58 @@
 import flet as ft
+import json
+import db
 
 def main(page:ft.Page):
-    page.add(ft.Text("Hello"))
+    page.title = "Problem Set"
+
+    statement_text = ft.Text(size=20)
+    result_text = ft.Text(size=15)
+    options_column = ft.Column()
+
+    current_problem = None 
+
+    def load_new_problem():
+        nonlocal current_problem
+        current_problem = db.get_random_unsolved_problem()
+        result_text.value = ""
+
+        if current_problem is None:
+            statement_text.value = "No unsolved problems left!"
+            options_column.controls = []
+            page.update()
+            return
+
+        statement_text.value = current_problem["statement"]
+        options = json.loads(current_problem["options"])
+
+        buttons = []
+
+        for opt in options:
+            label = opt["value"] if opt["type"] == "text" else f"[Image: {opt["value"]}]"
+            buttons.append(
+                ft.ElevatedButton(
+                    f"{opt['id']}: {label}",
+                    on_click=lambda e, opt_id=opt["id"]: check_answer(opt_id)
+                )
+            )
+        options_column.controls = buttons
+        page.update()
+
+    def check_answer(selected_option_id):
+        is_correct = db.check_solution(current_problem["id"],selected_option_id)
+        if is_correct:
+            result_text.value = "Correct"
+            result_text.color = ft.Colors.GREEN 
+        else:
+            result_text.value = "Wrong. Try again."
+            result_text.color = ft.Colors.RED 
+        page.update()
+    
+    next_button = ft.ElevatedButton("Next problem", on_click=lambda e: load_new_problem())
+
+    page.add(statement_text, options_column, result_text, next_button)
+
+    load_new_problem()
+
 
 ft.app(target=main)
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 def get_connection():
     conn = sqlite3.connect("problems.db")
@@ -23,9 +24,53 @@ def init_db():
             problem_id INTEGER PRIMARY KEY,
             solved BOOLEAN DEFAULT 0,
             attempts INTEGER DEFAULT 0,
-            first_solved_at TIMESTAMP,
+            first_attempt_correct BOOLEAN DEFAULT NULL
             FOREIGN KEY(problem_id) REFERENCES problems(id)
         )
     """)
     conn.commit()
     conn.close()
+
+def get_random_unsolved_problem():
+    conn = get_connection()
+    rows = conn.execute("""
+        SELECT * FROM problems, problem_progress WHERE problem_progress.problem_id = problems.id AND problem_progress.solved = 0
+    """).fetchall()
+    conn.close()
+    if not rows:
+        return None
+    return dict(random.choice(rows))
+
+def check_solution(problem_id, student_answer):
+    conn = get_connection()
+    conn.execute("""
+       UPDATE problem_progress
+       SET attempts = attempts + 1
+       WHERE problem_id = ?          
+    """,(problem_id,))
+    answer = conn.execute("SELECT answer FROM problems WHERE id = ?", (problem_id,)).fetchone()
+    answer = answer["answer"]
+    attempts = conn.execute("SELECT attempts FROM problem_progress WHERE problem_id = ?",(problem_id,)).fetchone()
+    attempts = attempts["attempts"]
+    if answer == student_answer:
+        conn.execute("""
+            UPDATE problem_progress
+            SET solved = True
+            WHERE problem_id = ?
+        """,(problem_id,))
+        if attempts == 1:
+            conn.execute("""
+                UPDATE problem_progress
+                SET first_attempt_correct = True
+                WHERE problem_id = ?
+            """,(problem_id,))
+    else:
+        if attempts == 1:
+            conn.execute("""
+                UPDATE problem_progress
+                SET first_attempt_correct = False
+                WHERE problem_id = ?
+            """,(problem_id,))
+    conn.commit()
+    conn.close()
+    return answer == student_answer 
